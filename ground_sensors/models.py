00555b1b263daa1126e267caa4268f6ac95a5d74
AiltonFidelix <ailton1626@gmail.com>
2021-10-03 11:51:05 -0300

    Change device num of chars

--- ground_sensors/models.py
+++ ground_sensors/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class GroundSensor(models.Model):
     """Model from ground sensor API"""
 
-    device = models.CharField(max_length=10, primary_key=True)
+    device = models.CharField(max_length=16, primary_key=True)
     soil_humidity = models.IntegerField()
     air_humidity = models.IntegerField()
     temperature = models.FloatField()
